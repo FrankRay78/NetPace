e7f1d0d71dd5c8beb60fb6751c25631b48c4ac04
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-04-14 22:23:06 +0100

    Example project for nuget consumers of NetPace.Core

---
+++ examples/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using NetPace.Core;
+using NetPace.Core.Clients;
+
+
+// Instantiate a speed tester
+var speedTester = new OoklaSpeedtest() as ISpeedTestService;
+
+// Get a list of available speed test servers
+var servers = await speedTester.GetServersAsync();
+
+// Get the fastest speed test server
+var fastestServer = await speedTester.GetFastestServerByLatencyAsync(servers) ?? default;
+
+// Display the sever name and latency
+Console.WriteLine($"{fastestServer.server.Sponsor} ({fastestServer.latency} ms)");
