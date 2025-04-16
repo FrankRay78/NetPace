12cb65d34ea9a7100c6c4f455bb1827220cf3d7b
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-04-16 20:07:44 +0100

    Minor updates

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -2,8 +2,12 @@
 using NetPace.Core.Clients;
 
 
+// The following example code demonstrates how to use NetPace.Core library.
+// Error handling, null and empty array checks have been omitted for brevity.
+
 // Instantiate a speed tester
-var speedTester = new OoklaSpeedtest() as ISpeedTestService;
+//var speedTester = new OoklaSpeedtest() as ISpeedTestService;
+var speedTester = new SpeedTestStub() as ISpeedTestService;
 
 // Get a list of available speed test servers
 var servers = await speedTester.GetServersAsync();
