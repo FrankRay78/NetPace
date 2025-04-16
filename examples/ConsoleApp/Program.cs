094476fca63b9270f753de1861da7a9fcb5bd72e
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-04-16 21:23:51 +0100

    Example console app completed.

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,9 +1,12 @@
 using NetPace.Core;
 using NetPace.Core.Clients;
+using NetPace.Core.DataTypes;
+using NetPace.Core.Extensions;
 
 
 // The following example code demonstrates how to use NetPace.Core library.
 // Error handling, null and empty array checks have been omitted for brevity.
+
 
 // Instantiate a speed tester
 //var speedTester = new OoklaSpeedtest() as ISpeedTestService;
@@ -17,3 +20,15 @@ var fastestServer = await speedTester.GetFastestServerByLatencyAsync(servers) ??
 
 // Display the sever name and latency
 Console.WriteLine($"{fastestServer.server.Sponsor} ({fastestServer.latency} ms)");
+
+// Perform download speed test
+var downloadResult = await speedTester.GetDownloadSpeedAsync(fastestServer.server);
+
+// Display the download speed
+Console.WriteLine($"Download: {downloadResult.GetSpeedString(SpeedUnit.BitsPerSecond, SpeedUnitSystem.SI)}");
+
+// Perform upload speed test
+var uploadResult = await speedTester.GetUploadSpeedAsync(fastestServer.server);
+
+// Display the upload speed
+Console.WriteLine($"Upload: {uploadResult.GetSpeedString(SpeedUnit.BitsPerSecond, SpeedUnitSystem.SI)}");
