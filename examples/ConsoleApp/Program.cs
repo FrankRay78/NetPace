0067832651411beec4912ba4d69237808d8a605a
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-04-30 18:11:17 +0100

    Improved warning and simplified example code.

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,31 +1,22 @@
+// This example omits exception handling for brevity. However, because network operations
+// are inherently unreliable, you should assume that every method call may throw.
+// NetPace.Core propagates all exceptions to the caller; handle them appropriately.
+
 using NetPace.Core;
 using NetPace.Core.Clients.Ookla;
 
-
-// The following example code demonstrates how to use NetPace.Core library.
-// Error handling, null and empty array checks have been omitted for brevity.
-
-
-// Instantiate a speed tester
+// Create an Ookla network speed test client.
 var speedTester = new OoklaSpeedtest() as ISpeedTestService;
 
-// Get a list of available speed test servers
+// Get the fastest server from those available.
 var servers = await speedTester.GetServersAsync();
+var fastest = await speedTester.GetFastestServerByLatencyAsync(servers);
 
-// Get the fastest speed test server
-var fastestServer = await speedTester.GetFastestServerByLatencyAsync(servers);
+// Perform download and upload speed tests.
+var downloadResult = await speedTester.GetDownloadSpeedAsync(fastest.Server);
+var uploadResult = await speedTester.GetUploadSpeedAsync(fastest.Server);
 
-// Display the fastest server and latency
-Console.WriteLine($"{fastestServer.Server.Sponsor} ({fastestServer.Latency} ms)");
-
-// Perform download speed test
-var downloadResult = await speedTester.GetDownloadSpeedAsync(fastestServer.Server);
-
-// Display the download speed
+// Display the server, latency, download and upload speeds.
+Console.WriteLine($"{fastest.Server.Sponsor} ({fastest.Latency} ms)");
 Console.WriteLine($"Download: {downloadResult.GetSpeedString(SpeedUnit.BitsPerSecond, SpeedUnitSystem.SI)}");
-
-// Perform upload speed test
-var uploadResult = await speedTester.GetUploadSpeedAsync(fastestServer.Server);
-
-// Display the upload speed
 Console.WriteLine($"Upload: {uploadResult.GetSpeedString(SpeedUnit.BitsPerSecond, SpeedUnitSystem.SI)}");
