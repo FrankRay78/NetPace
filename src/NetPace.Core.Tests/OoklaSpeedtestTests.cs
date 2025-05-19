14ddb292b4eb28ff4a02ff3e87b969ac5a0dd4fc
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-05-19 12:40:17 +0100

    Initial OoklaSpeedtest tests, using mock

---
+++ src/NetPace.Core.Tests/OoklaSpeedtestTests.cs
@@ -0,0 +1,76 @@
+using NetPace.Core.Clients.Ookla;
+using RichardSzalay.MockHttp;
+
+namespace NetPace.Core.Tests;
+
+public class OoklaSpeedtestTests
+{
+    [Fact]
+    public async Task GetServersAsync_ShouldReturnSingleServer_WhenResponseHasOneServer()
+    {
+        // Given
+        var fakeXml = """
+        <settings>
+            <servers>
+                <server id="1" url="http://testurl.com" lat="0" lon="0" name="TestLocation" country="TestCountry" sponsor="TestSponsor"/>
+            </servers>
+        </settings>
+        """;
+
+        var mockHttp = new MockHttpMessageHandler();
+        mockHttp.When("http://*")
+                .Respond("application/xml", fakeXml);
+
+        var httpClient = mockHttp.ToHttpClient();
+        var speedtest = new OoklaSpeedtest(httpClientOverride: httpClient);
+
+        // When
+        var servers = await speedtest.GetServersAsync();
+
+        // Then
+        Assert.Single(servers);
+        Assert.Equal("TestLocation", servers[0].Location);
+        Assert.Equal("TestSponsor", servers[0].Sponsor);
+        Assert.Equal("http://testurl.com", servers[0].Url);
+    }
+
+    [Fact]
+    public async Task GetServersAsync_ShouldReturnMultipleServers_WhenResponseHasMultipleServers()
+    {
+        // Given
+        var fakeXml = """
+        <settings>
+            <servers>
+                <server id="1" url="http://testurl1.com" lat="0" lon="0" name="Location1" country="Country1" sponsor="Sponsor1"/>
+                <server id="2" url="http://testurl2.com" lat="1" lon="1" name="Location2" country="Country2" sponsor="Sponsor2"/>
+            </servers>
+        </settings>
+        """;
+
+        var mockHttp = new MockHttpMessageHandler();
+        mockHttp.When("http://*")
+                .Respond("application/xml", fakeXml);
+
+        var httpClient = mockHttp.ToHttpClient();
+        var speedtest = new OoklaSpeedtest(httpClientOverride: httpClient);
+
+        // When
+        var servers = await speedtest.GetServersAsync();
+
+        // Then
+        Assert.Equal(2, servers.Length);
+        Assert.Collection(servers,
+            s =>
+            {
+                Assert.Equal("Location1", s.Location);
+                Assert.Equal("Sponsor1", s.Sponsor);
+                Assert.Equal("http://testurl1.com", s.Url);
+            },
+            s =>
+            {
+                Assert.Equal("Location2", s.Location);
+                Assert.Equal("Sponsor2", s.Sponsor);
+                Assert.Equal("http://testurl2.com", s.Url);
+            });
+    }
+}
