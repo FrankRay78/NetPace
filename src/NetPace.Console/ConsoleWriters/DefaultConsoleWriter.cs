7bdd53d70b81d3063f9a0720fb2108f00a383fae
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-12-02 18:29:06 +0000

    Progress spinner shows when fetching the fastest speed test server to use

--- src/NetPace.Console/ConsoleWriters/DefaultConsoleWriter.cs
+++ src/NetPace.Console/ConsoleWriters/DefaultConsoleWriter.cs
@@ -9,11 +9,25 @@ public sealed class DefaultConsoleWriter : IConsoleWriter
     public async Task PerformSpeedTestAsync(bool initialSpeedTest, IAnsiConsole console, IClock clock, ISpeedTestService speedTestClient, SpeedTestCommandSettings settings, CancellationToken cancellationToken)
     {
         // Get the server to use for speed testing.
-        var fastest = await console.Status()
-            .AutoRefresh(true)
-            .StartAsync("Choosing server", async ctx =>
+        var fastest = await console.Progress()
+            .AutoClear(true)
+            .Columns(
+            [
+                new SpinnerColumn(),
+                new TaskDescriptionColumn(),
+            ])
+            .StartAsync(async progress =>
             {
-                return await ServerSelector.GetServerAsync(speedTestClient, settings, cancellationToken);
+                var fastestServerProgress = progress.AddTask("Choosing server", autoStart: true, maxValue: 100);
+
+                try
+                { 
+                    return await ServerSelector.GetServerAsync(speedTestClient, settings, cancellationToken);
+                }
+                finally
+                { 
+                    fastestServerProgress.StopTask();
+                }
             });
 
 
